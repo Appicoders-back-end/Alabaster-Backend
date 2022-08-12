c75e8ce78791f48717e311f8c37e872fa6424409
Irfan <hirfan875@gmail.com>
2022-08-12 18:58:23 +0500

    12-aug-2022 commit

--- app/Http/Resources/Contractor/Jobs/JobsDetailResource.php
+++ app/Http/Resources/Contractor/Jobs/JobsDetailResource.php
@@ -18,15 +18,19 @@ class JobsDetailResource extends JsonResource
         return [
             'id' => $this->id,
             'date' => $this->date,
-            'time' => $this->time,
-            'datetime' => $this->date_time,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time,
             'customer_id' => $this->customer ? $this->customer->id : null,
             'customer_name' => $this->customer ? $this->customer->name : null,
             'customer_image' => $this->customer ? $this->customer->profile_image : null,
+            'cleaner_id' => $this->cleaner ? $this->cleaner->id : null,
+            'cleaner_name' => $this->cleaner ? $this->cleaner->name : null,
+            'cleaner_image' => $this->cleaner ? $this->cleaner->profile_image : null,
             'location' => $this->location ? new AddressesResource($this->location) : null,
             'service_name' => $this->category ? $this->category->name : null,
             'urgency' => $this->urgency,
             'details' => $this->details,
+            'status' => $this->status,
         ];
     }
 }
