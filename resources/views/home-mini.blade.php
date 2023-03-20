a0956972b2c6ab644f51011aa5717769eedc587c
Irfan <hirfan875@gmail.com>
2023-03-20 18:18:58 +0500

    fixes

---
+++ resources/views/home-mini.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Alabaster - Mobile App</title>
+    <meta name="description" content="Alabaster - The Perfect Solution For All Your Needs! ">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" href="assets/images/favicon.png">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <!-- Google Font (font-family: 'Roboto', sans-serif;) -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet">
+    <!-- Google Font ('Poppins', sans-serif;) -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+<!-- Wrapper -->
+<div id="wrapper" class="wrapper">
+    <!-- Header Area -->
+    <div class="tm-header tm-header-fixed tm-sticky-header">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-3 col-md-9 col-9">
+                    <a class="tm-header-logo" href="{{route('home')}}">
+                        <img src="assets/images/logo-white.png" alt="alabaster logo">
+                    </a>
+                </div>
+            </div>
+            <div class="tm-mobilenav"></div>
+        </div>
+    </div>
+    <!--// Header Area -->
+    <!-- Heroslider Area -->
+    <div id="tm-area-heroslider" class="tm-heroslider">
+        <div class="tm-heroslider-inner" style="background: -webkit-linear-gradient(90deg, #05050d 0%, #05050d 100%);">
+            <img src="assets/images/heroslider-overlay-shape.png" alt="heroslider ovelay" class="tm-heroslider-ovelayshape">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-8 col-lg-7 order-2 order-lg-1">
+                        <div class="tm-heroslider-content">
+                            <div class="tm-buttongroup">
+                                {{--                                <a href="https://play.google.com/store/apps/details?id=com.app.alabaster&hl=en_IN" class="tm-button tm-button-lg tm-button-white tm-button-transparent"><i
+                                        class="zmdi zmdi-android"></i><span>Google Play</span></a>
+                                <a href="https://apps.apple.com/pk/app/alabaster/id6443721925" class="tm-button tm-button-lg tm-button-white tm-button-transparent"><i
+                                        class="zmdi zmdi-apple"></i><span>IOS App Store</span></a> --}}
+
+                                <a href="https://play.google.com/store/apps/details?id=com.app.alabaster&hl=en_IN">
+                                    <img src="assets/images/google.png" alt="Playstore logo" class="mobile-btn-size">
+                                </a>
+                                <a href="https://apps.apple.com/pk/app/alabaster/id6443721925">
+                                    <img src="assets/images/apple.png" alt="Appstore logo"  class="mobile-btn-size">
+                                </a>
+                            </div>
+                            <a href="#tm-area-features" class="tm-heroslider-scrolldown">
+                                <i class="zmdi zmdi-square-down"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-5 order-1 order-lg-2">
+                        <div class="tm-heroslider-mobileshow">
+                            <img src="assets/images/heroslider-mobile-show.png" alt="alabaster mobile">
+                            <div class="tm-heroslider-mobileshowanim">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--// Heroslider Area -->
+</div>
+<!--// Wrapper -->
+<!-- Google Map -->
+<!-- Js Files -->
+<script src="assets/js/modernizr-3.6.0.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<!--// Js Files -->
+</body>
+</html>
