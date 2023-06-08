17ea0469f0e8324fe959457452ed9e806b00bd2b
Irfan <hirfan875@gmail.com>
2023-06-08 17:08:22 +0500

    fixes

---
+++ database/migrations/2023_06_08_120335_change_contact_us_message_type.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contact_us', function (Blueprint $table) {
+            DB::statement("ALTER TABLE `contact_us` CHANGE `message` `message` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contact_us', function (Blueprint $table) {
+            $table->string('message')->nullable();
+        });
+    }
+};
